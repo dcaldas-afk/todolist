973aa89c4453f8dbac71b783559a77c6ca198cb0
Décio Leite <73242641+dcaldas-afk@users.noreply.github.com>
2023-10-16 15:56:59 -0300

    Update files

---
+++ src/main/java/br/com/decioleite/todolist/task/TaskModel.java
@@ -0,0 +1,44 @@
+package br.com.decioleite.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+    /* 
+     * ID
+     * Usuário
+     * Descrição
+     * Título
+     * Data de início
+     * Data de término
+     * Prioridade
+     * 
+     */
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endat;
+    private String priority;
+    
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
