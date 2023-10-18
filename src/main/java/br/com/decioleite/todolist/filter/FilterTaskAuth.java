3696eac20379e313abee6f5671a8bb47f4c16617
Décio Leite <73242641+dcaldas-afk@users.noreply.github.com>
2023-10-18 00:19:49 -0300

    Update files

--- src/main/java/br/com/decioleite/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/decioleite/todolist/filter/FilterTaskAuth.java
@@ -9,11 +9,11 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import br.com.decioleite.todolist.user.IUserRepository;
-import jakarta.servlet.Filter;
+//import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+//import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -25,41 +25,45 @@ public class FilterTaskAuth extends OncePerRequestFilter{
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
 
-        // Pegar a autenticação
-        var authorization = request.getHeader("Authorization");
-        System.out.println("Authorization");
-        System.out.println(authorization);
-
-        var authEncoded = authorization.substring("Basic".length()).trim();
-
-        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
-
-        var authString = new String(authDecode);
-
-        System.out.println("Authorization");
-        System.out.println(authString);
-
-        String[] credentials = authString.split(":");
-        String username = credentials[0];
-        String password = credentials[1];
-       
-        // Validar usuário
-        var user = this.userRepository.findByUsername(username);
-        if (user == null) {
-            response.sendError(401);
-        } else {
-
-            // Validar senha
-            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-            if(passwordVerify.verified) {
-                filterChain.doFilter(request, response);
-            } else {
+        var servletPath = request.getServletPath();
+        if(servletPath.startsWith("/tasks/")) {
+            // Pegar a autenticação
+            var authorization = request.getHeader("Authorization");
+            System.out.println("Authorization");
+            System.out.println(authorization);
+            var authEncoded = authorization.substring("Basic".length()).trim();
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+            var authString = new String(authDecode);
+            System.out.println("Authorization");
+            System.out.println(authString);
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+                
+            // Validar usuário
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
                 response.sendError(401);
-            }
-            
+            } else {
 
+                // Validar senha
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if(passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+                // Continua
+       
+            }
+
+        } else {
+            filterChain.doFilter(request, response);
         }
+
     }
+
 }
--- src/main/java/br/com/decioleite/todolist/task/TaskController.java
+++ src/main/java/br/com/decioleite/todolist/task/TaskController.java
@@ -1,10 +1,22 @@
 package br.com.decioleite.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.config.Task;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.decioleite.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,9 +26,31 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    private TaskModel create(@RequestBody TaskModel TaskModel) {
-       System.out.println("Chegou no controller");
+    public ResponseEntity create(@RequestBody TaskModel TaskModel, HttpServletRequest request) {
+       System.out.println("Chegou no controller " + request.getAttribute("idUser"));
+       var idUser = request.getAttribute("idUser");
+       TaskModel.setIdUser((UUID)idUser);
+       var currentDate = LocalDateTime.now();
+       if(currentDate.isAfter(TaskModel.getStartAt()) || currentDate.isAfter(TaskModel.getEndat()))
+            return ResponseEntity.status(400).body("A data de início e de fim devem ser maior do que a data atual");
+       if (TaskModel.getStartAt().isAfter(TaskModel.getEndat()) || TaskModel.getEndat().isEqual(TaskModel.getStartAt()))
+            return ResponseEntity.status(400).body("A data de fim deve ser maior que a data de início");
        var task = this.taskRepository.save(TaskModel);
-       return task;
+       return ResponseEntity.status(200).body(task);
+    }
+
+    @GetMapping("/")
+    public java.util.List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID)idUser);
+        return tasks;
+    }
+
+    // .../tasks/"idUSER"
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel TaskModel, HttpServletRequest request, @PathVariable UUID id) {
+        var task = this.taskRepository.findById(id).orElse(null);
+        Utils.copyNonNullProperties(TaskModel, task);
+        return this.taskRepository.save(task);
     }
 }
